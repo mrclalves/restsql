23ec0e6024a42f5693fa04e32fa2ee9da3d16d83
marcelo.alves <marcelo.alves@objetivait.com.br>
2017-11-28 22:18:28 -0200

    adding apache shiro to security

--- src/org/restsql/service/RestSqlApplication.java
+++ src/org/restsql/service/RestSqlApplication.java
@@ -6,8 +6,14 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import org.apache.shiro.mgt.SubjectFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.SubjectContext;
 import org.restsql.core.Config;
 import org.restsql.service.monitoring.MonitoringFactory;
+import org.secnod.shiro.jersey.AuthInjectableProvider;
+import org.secnod.shiro.jersey.ShiroResourceFilterFactory;
+import org.secnod.shiro.jersey.SubjectInjectableProvider;
 
 /**
  * Identifies JAX-RS resources through code, since the declarative Jersey scanner does not work with JBoss AS. 
@@ -28,12 +34,24 @@ public class RestSqlApplication extends Application {
 		classes.add(ResResource.class);
 		classes.add(WadlResource.class);
 		classes.add(ToolsResource.class);
+		
 		return classes;
 	}
 
 	@Override
 	public Set<Object> getSingletons() {
 		try {
+			Set<Object> singletons = MonitoringFactory.getMonitoringManager().getApplicationSingletons();
+			singletons.add(new ShiroResourceFilterFactory());
+			singletons.add(new SubjectFactory() {
+				
+				@Override
+				public Subject createSubject(SubjectContext arg0) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+			});
+			
 			return MonitoringFactory.getMonitoringManager().getApplicationSingletons();
 		} catch (Throwable throwable) {
 			Config.logger.error(String.format(
